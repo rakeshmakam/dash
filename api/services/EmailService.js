ddd2c6c37b0201958fbf9e05cb822348895d1718
dhananjay-weds-vanishree <vani.jay26@gmail.com>
2015-05-12 14:09:38 +0530

    Email Service email id changed and Invitation js URL modified

--- api/services/EmailService.js
+++ api/services/EmailService.js
@@ -5,8 +5,8 @@ var hbs = require('nodemailer-express-handlebars');
 var options = {
     service: 'Gmail',
     auth: {
-        user: 'm.balu0007@gmail.com',
-        pass: 'balamovvar'
+        user: 'mantradash@gmail.com',
+        pass: 'dash@123'
     }
 }
 
@@ -15,7 +15,7 @@ transporter.use('compile', hbs({viewEngine: 'ejs', viewPath: 'views', extName: '
 
 exports.send = function(data, cb) {
 	var mail = {
-		from: 'Dash <noreply@dash.com>',
+		// from: 'Dash <noreply@dash.com>',
 		to: data.email,
 		subject: 'Dash invitation',
 		template: 'invitation',
