f7ec9bbeca5635fc595416aaca4f199f21a73b28
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2015-06-08 14:24:54 +0530

    added policy to check activity deleting

--- api/policies/isOwner.js
+++ api/policies/isOwner.js
@@ -10,17 +10,12 @@ module.exports = function(req, res, next) {
 
   var activityId = req.param('id');
   var userId = req.session.user.id;
-  console.log('a',userId);
   Activity.findOne({id : activityId}).exec(function(err,activity){
   	if (!err){
-  		console.log(activity);
   		var ownerId = activity.user;
-  		console.log('b',ownerId);
   		if (userId == ownerId){
-  			console.log('in');
   			return next();
   		} else {
-  			console.log('out');
 	  		return res.forbidden('You are not permitted to perform this action.');
 	  	}
   	} else {
